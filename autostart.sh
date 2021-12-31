4d3319363efd5bb5c466e8a27f5781529074da9e
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2021-12-31 05:23:42 -0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,14 +2,15 @@
 # wallpaper
 ~/.fehbg &
 
-# statusbar
-$HOME/.dwm/dwm-bar/dwm_bar.sh &
+# dwmblocks
+export PATH="$PATH:$HOME/.dwm/blocks"
+dwmblocks &
 
 # st
 /usr/local/bin/st -e /bin/bash --login &
 
 # fm
-pcmanfm &
+#pcmanfm &
 
 # numlock
 numlockx &
