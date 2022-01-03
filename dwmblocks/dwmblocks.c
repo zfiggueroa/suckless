aa2007bda37e6de90dae7305daf82499a7a94575
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2022-01-03 15:18:58 -0700

    Update dwmblocks.c

--- dwmblocks/dwmblocks.c
+++ dwmblocks/dwmblocks.c
@@ -91,7 +91,7 @@ void getcmd(const Block *block, char *output)
 	if (delim[0] != '\0') {
 		//only chop off newline if one is present at the end
 		i = output[i-1] == '\n' ? i-1 : i;
-		strncpy(output+i, delim, delimLen); 
+		strncpy(output+i, delim, delimLen);
 	}
 	else
 		output[i++] = '\0';
