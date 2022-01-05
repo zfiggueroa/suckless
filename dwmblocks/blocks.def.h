f654c97623fead7f71b20498758cdc5f911da478
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2022-01-04 20:44:18 -0700

    Update blocks.def.h

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,10 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*	{"",		"nettraf",		1,					0},			   */
+	{"",		"news",			600,				0},
 	{"", 		"memory",		2,					0},
 	{"",		"clock",		60,					0},
 	{"",		"volume",		1,					0},
-	{"",		"internet",		10,					0},
+/*	{"",		"internet",		10,					0},			   */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
