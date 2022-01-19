e376a3b29007ba205df9781a3d53957e80e494f2
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2022-01-19 14:38:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #/bin/sh
 
-# download files
-cd $HOME/; git clone https://github.com/zfiggueroa/dwm-patched; clear
-mv $HOME/dwm-patched $HOME/.dwm; rm -rf $HOME/.dwm/.git/
+# clone repo
+cd $HOME/; git clone https://github.com/zfiggueroa/.dwm; clear
 
 # dwm
 cd $HOME/.dwm/dwm
+sudo cp config.def.h config.h
+sudo make clean install
+
+# dmenu
+cd $HOME/.dwm/dmenu
 sudo cp config.def.h config.h
 sudo make clean install
 
@@ -14,18 +18,19 @@ cd $HOME/.dwm/dwmblocks
 sudo cp blocks.def.h blocks.h
 sudo make clean install
 
-# dmenu
-cd $HOME/.dwm/dmenu
-sudo cp config.def.h config.h
-sudo make clean install
-
 # st
 cd $HOME/.dwm/st
 sudo cp config.def.h config.h
 sudo make clean install
 
 # binaries
-chmod -R +x $HOME/.dwm/modules/*
+cd $HOME/.dwm/modules/; ls | grep -v colors | grep -v readme.md | xargs -I {} chmod -R +x {}
+
+# xsession
+sudo mv $HOME/.dwm/xsession /usr/share/xsessions/dwm-session.desktop
 
 # autorun
 chmod +x $HOME/.dwm/autostart
+
+# notify user
+notify-send -t 0 "dwm installation completed"
