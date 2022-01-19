61acb62e305549a6e06b5ac0fcba793ce257697a
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2022-01-19 15:45:56 -0700

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,19 +1,14 @@
-/* See LICENSE file for copyright and license details. */
+// See LICENSE file for copyright and license details.
 
 #include <X11/XF86keysym.h>
 
-/* volume */
-static const char *upvol[]	 = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",	 NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",	 NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",	  "0", "toggle", NULL };
-
-/* appearance */
-static const unsigned int borderpx	= 0;	/* border pixel of windows */
-static const unsigned int gappx		= 5;	/* gaps between windows */
-static const unsigned int snap		= 32;	/* snap pixel */
-static const int showbar			= 1;	/* 0 means no bar */
-static const int topbar				= 1;	/* 0 means bottom bar */
-static const char *fonts[]			= { "monospace:size=10" };
+// appearance
+static const unsigned int borderpx	= 0;	// border pixel of windows
+static const unsigned int gappx		= 5;	// gaps between windows
+static const unsigned int snap		= 32;	// snap pixel
+static const int showbar			= 1;	// 0 means no bar
+static const int topbar				= 1;	// 0 means bottom bar
+static const char *fonts[]			= {"Font Awesome 5 Free:style=Solid:pixelsize=10:antialias=true","monospace:size=10"};
 static const char dmenufont[]		= "monospace:size=10";
 static const char col_gray1[]		= "#222222";
 static const char col_gray2[]		= "#444444";
@@ -21,47 +16,49 @@ static const char col_gray3[]		= "#bbbbbb";
 static const char col_gray4[]		= "#eeeeee";
 static const char col_cyan[]		= "#5895ad";
 static const char *colors[][3]		= {
-	/*				 fg			bg		   border	 */
+	//				 fg			bg		   border
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging	https://fontawesome.com/v6.0/icons	*/
+// tagging	https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=solid&m=free
 
-static const char *tags[] = { "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class		instance	title			mask		switch	isfloating	 monitor*/
-	{ "Pcmanfm",	NULL,		NULL,			1 << 1,		1,		0,			-1 },
+	* WM_CLASS(STRING) = instance, class
+	* WM_NAME(STRING) = title
+	* class			instance	title			mask		switch	isfloating	 monitor */
+	{ "st",			NULL,		"lf",			1 << 1,		1,		0,			-1 },
 	{ "feh",		NULL,		NULL,			1 << 1,		1,		0,			-1 },
-	{ "st",			"st", 		"cmus v2.9.1",	1 << 2,		1,		0,			-1 },
+	{ "st",			NULL, 		"cmus",			1 << 2,		1,		0,			-1 },
 	{ "mpv",		NULL,		NULL,			1 << 3,		1,		1,			-1 },
 	{ "retroarch",	NULL,		NULL,			1 << 4,		1,		0,			-1 },
-	{ NULL,			NULL,		"DeSmuME",		1 << 4,		1,		0,			-1 },
-	{ NULL,			"Steam",	NULL,			1 << 4,		0,		0,			-1 },
+	{ NULL,			"Steam",	NULL,			1 << 4,		1,		0,			-1 },
 	{ "firefox",	NULL,		NULL,			1 << 5,		1,		0,			-1 },
-	{ NULL,			"geany",	NULL,			1 << 6,		1,		0,			-1 },
 	{ NULL,			NULL, 		"BleachBit",	1 << 6,		1,		0,			-1 },
-	{ "st",			"st",		"st",			0,			1,		0,			-1 },
+	{ "st",			NULL,		"vim",			1 << 6,		1,		0,			-1 },
+	{ NULL,			"geany",	NULL,			1 << 6,		1,		0,			-1 },
+	{ "AQEMU",		NULL,		NULL,			1 << 6,		1,		0,			-1 },
+	{ "Qemu-",		NULL,		NULL,			1 << 7,		1,		0,			-1 },
+	{ "st",			NULL,		"newsboat",		1 << 7,		1,		0,			-1 },
+	{ "st",			NULL,		"/bin/sh",		0,			1,		0,			-1 },
 };
 
-/* layout(s) */
-static const float mfact	 = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster	 = 1;	 /* number of clients in master area */
-static const int resizehints = 1;	 /* 1 means respect size hints in tiled resizals */
+// layout(s)
+static const float mfact	 = 0.55;	// factor of master area size [0.05..0.95]
+static const int nmaster	 = 1;		// number of clients in master area
+static const int resizehints = 1;		// 1 means respect size hints in tiled resizals
 
 static const Layout layouts[] = {
-	/* symbol	arrange function */
+	// symbol	arrange function
 	{ "|",		tile },
 	{ "-",		NULL },
 	{ "m",		monocle },
 };
 
-/* key definitions */
+// key definitions
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,						KEY,	view,		{.ui = 1 << TAG} }, \
@@ -69,21 +66,24 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,				KEY,	tag,		{.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask,	KEY,	toggletag,	{.ui = 1 << TAG} },
 
-//* helper for spawning shell commands in the pre dwm-5.0 fashion */
+// dwmblocks
+#define STATUSBAR "dwmblocks"
+
+// shell command helper
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+// commands
+static char dmenumon[2] = "0"; // component of dmenucmd, manipulated in spawn()
 static const char *dmenucmd[] = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
-	/* modifier				key			function		argument */
-	{ MODKEY,				XK_F12,		spawn,			{.v = upvol   } },
-	{ MODKEY,				XK_F11,		spawn,			{.v = downvol } },
-	{ MODKEY,				XK_F9,		spawn,			{.v = mutevol } },
+	// modifier				key			function		argument
+	{ MODKEY,				XK_F12,		spawn,			SHCMD("pamixer --allow-boost -i 5; kill -39 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_F11,		spawn,			SHCMD("pamixer --allow-boost -d 5; kill -39 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_F9,		spawn,			SHCMD("pamixer -t; kill -39 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_F10,		spawn,			SHCMD("st -e cmus") },
+	{ MODKEY|ShiftMask,		XK_Return,	spawn,			SHCMD("st -e /bin/sh --login") },
 	{ MODKEY,				XK_p,		spawn,			{.v = dmenucmd } },
-	{ MODKEY|ShiftMask,		XK_Return,	spawn,			{.v = termcmd } },
 	{ MODKEY,				XK_b,		togglebar,		{0} },
 	{ MODKEY,				XK_j,		focusstack,		{.i = +1 } },
 	{ MODKEY,				XK_k,		focusstack,		{.i = -1 } },
@@ -120,18 +120,20 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_q,		quit,			{0} },
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* button definitions
+ * click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click			event mask	button		function		argument */
-	{ ClkLtSymbol,		0,			Button1,	setlayout,		{0} },
-	{ ClkLtSymbol,		0,			Button3,	setlayout,		{.v = &layouts[2]} },
-	{ ClkStatusText,	0,			Button2,	spawn,			{.v = termcmd } },
-	{ ClkClientWin,		MODKEY,		Button1,	movemouse,		{0} },
-	{ ClkClientWin,		MODKEY,		Button2,	togglefloating,	{0} },
-	{ ClkClientWin,		MODKEY,		Button3,	resizemouse,	{0} },
-	{ ClkTagBar,		0,			Button1,	view,			{0} },
-	{ ClkTagBar,		0,			Button3,	toggleview,		{0} },
-	{ ClkTagBar,		MODKEY,		Button1,	tag,			{0} },
-	{ ClkTagBar,		MODKEY,		Button3,	toggletag,		{0} },
+	// click event		mask	button		function		argument
+	{ ClkLtSymbol,		0,		Button1,	setlayout,		{0} },
+	{ ClkLtSymbol,		0,		Button3,	setlayout,		{.v = &layouts[2]} },
+	{ ClkStatusText,	0,		Button1,	sigstatusbar,	{.i = 1} },
+	{ ClkStatusText,	0,		Button2,	sigstatusbar,	{.i = 2} },
+	{ ClkStatusText,	0,		Button3,	sigstatusbar,	{.i = 3} },
+	{ ClkClientWin,		MODKEY,	Button1,	movemouse,		{0} },
+	{ ClkClientWin,		MODKEY,	Button2,	togglefloating,	{0} },
+	{ ClkClientWin,		MODKEY,	Button3,	resizemouse,	{0} },
+	{ ClkTagBar,		0,		Button1,	view,			{0} },
+	{ ClkTagBar,		0,		Button3,	toggleview,		{0} },
+	{ ClkTagBar,		MODKEY,	Button1,	tag,			{0} },
+	{ ClkTagBar,		MODKEY,	Button3,	toggletag,		{0} },
 };
