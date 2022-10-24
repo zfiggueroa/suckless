a1c7d6c449a1a89340b685c2a89e5e464115f6b8
zfiggueroa <95833594+zfiggueroa@users.noreply.github.com>
2022-10-24 01:59:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,36 @@
 #/bin/sh
 
 # clone repo
-cd $HOME/; git clone https://github.com/zfiggueroa/Suckless; clear
-
-# rename
-mv $HOME/suckless/ $HOME/.suckless/
+cd $HOME/.local/; git clone https://github.com/zfiggueroa/suckless; clear
 
 # dwm
-cd $HOME/.suckless/dwm
+cd $HOME/.local/suckless/dwm
 sudo cp config.def.h config.h
 sudo make clean install
 
 # dmenu
-cd $HOME/.suckless/dmenu
+cd $HOME/.local/suckless/dmenu
 sudo cp config.def.h config.h
 sudo make clean install
 
 # dwmblocks
-cd $HOME/.suckless/dwmblocks
+cd $HOME/.local/suckless/dwmblocks
 sudo cp config.def.h config.h
 sudo make clean install
 
 # st
-cd $HOME/.suckless/st
+cd $HOME/.local/suckless/st
 sudo cp config.def.h config.h
 sudo make clean install
 
 # modules
-cd $HOME/.suckless/modules/; ls | grep -v resources | grep -v readme.md | xargs -I {} chmod -R +x {}
+cd $HOME/.local/suckless/modules/; ls | grep -v resources | grep -v readme.md | xargs -I {} chmod -R +x {}
 
 # xsession
-sudo mv $HOME/.suckless/xsession /usr/share/xsessions/dwm-session.desktop
+sudo mv $HOME/.local/suckless/xsession /usr/share/xsessions/dwm-session.desktop
 
 # autorun
-chmod +x $HOME/.suckless/autostart
+chmod +x $HOME/.local/suckless/autostart
 
 # notify user
-notify-send -t 0 "suckless installation completed"
+dunstify -i appset -t 0 -r 9856 -a install "suckless installation completed"
